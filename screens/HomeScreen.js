325b50c57874a41ea0d18978315e1c256d729357
Thibault-Franceus <r1006946@student.thomasmore.be>
2025-06-12 20:51:23 +0200

    feat: Made code look clean

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,24 +1,10 @@
 import React from "react";
 import { View, ScrollView, StyleSheet, TouchableOpacity, Text } from "react-native";
-import ProductCard from "../components/ProductCard";
 import { PrimaryButton, SecondaryButton } from "../components/AppButton";
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.overlayButton}
-        onPress={() => navigation.navigate("Products")}
-      >
-        <Text style={{ fontSize: 20, color: "Black" }}>View Products</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.overlayButton}
-        onPress={()=> navigation.navigate("Blogs")}
-      >
-      
-        <Text style={{ fontSize: 20, color: "Black" }}>View Blogs</Text>
-      </TouchableOpacity>
       <SecondaryButton
         title="Contact"
         onPress={() => navigation.navigate("Contact")}
