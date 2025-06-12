9afd5df3828b7d1d7193da467af1d8d20c980160
Thibault-Franceus <r1006946@student.thomasmore.be>
2025-06-12 12:11:11 +0200

    feat: made the API work so that products are loaded from the cms on webflow

--- screens/Products.js
+++ screens/Products.js
@@ -1,52 +1,76 @@
-import React from "react";
+import React, {use, useEffect, useState} from "react";
 import { View, ScrollView, StyleSheet, TouchableOpacity, Text } from "react-native";
 import ProductCard from "../components/ProductCard";
 
-const products = [
-  {
-    id: 1,
-    name: "Thibaults Favorit",
-    description: "€4,50",
-    image: "https://www.trappistwestmalle.be/wp-content/uploads/2023/03/Westmalle-Tripel-e1679067890610.png",
-  },
-  {
-    id: 2,
-    name: "Arthurs thick booty",
-    description: "€0",
-    image: "https://www.trappistwestmalle.be/wp-content/uploads/2023/03/Westmalle-Dubbel-e1679067974684-616x1024.png",
-  },
-  {
-    id: 3,
-    name: "Victors Favorite",
-    description: "€3,50",
-    image: "https://www.trappistwestmalle.be/wp-content/uploads/2023/03/Glas-met-fles-e1717663945824-608x1024.png",
-  },
-];
-
 const Products = ({ navigation }) => {
-  return (
-    <ScrollView contentContainerStyle={styles.container}>
-      {products.map((product) => (
-        <TouchableOpacity
-          key={product.id}
-          onPress={() => navigation.navigate('ProductDetail', { product })}
-        >
-          <ProductCard
-            image={product.image}
-            name={product.name}
-            description={product.description}
-          />
-        </TouchableOpacity>
-      ))}
-    </ScrollView>
-  );
-};
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(
+            "https://api.webflow.com/v2/sites/67b2f6b04b3860ceb9157814/products",
+            {
+                headers: {
+                    Authorization: 
+                    "Bearer 9459b25cd2659f2391c7510c4bb9084fc990188e2550f14528ebe65bb22f2b4c",
+                },
+                }
+        )
+        .then((res) => res.json())
+        .then((data) => 
+            setProducts(
+                data.items.map((item) => ({
+                    id: item.product.id,
+                    name: item.product.fieldData.name,
+                    price: (item.skus[0].fieldData.price.value || 0)/ 100,
+                    image: item.skus[0]?.fieldData["main-image"]?.url ,
+                    description: item.product.fieldData.description,
+                }))
+            )
+        )
+        .catch((error) => console.error("Error fetching products:", error));
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.heading}>Products</Text>
+            <ScrollView style={styles.cardContainer}>
+                <View style={styles.productList}>
+                    {products.map(((product) => (
+                        <TouchableOpacity
+                            key={product.id}
+                            onPress={() => navigation.navigate("ProductDetail", { product })}
+                        >
+                            <ProductCard
+                                image={product.image}
+                                name={product.name}
+                            />
+                        </TouchableOpacity>
+                    )))}
+                </View>
+            </ScrollView>
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-    alignItems: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        padding: 20,
+    },
+    heading: {
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 20,
+    },
+    cardContainer: {
+        flex: 1,
+    },
+    productList: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+    },
 });
 
 export default Products;
